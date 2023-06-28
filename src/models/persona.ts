bbbd437842c3c1a14b7dbbcd44b648bc7baa7a5c
AFPrograming <felipeandres2595@gmail.com>
2023-06-28 00:19:30 -0500

    last commit

--- src/models/persona.ts
+++ src/models/persona.ts
@@ -1,7 +1,7 @@
 import { DataTypes } from 'sequelize';
 import db from '../db/connection';
 
-const Persona = db.define('Persona', {
+const Persona = db.define('Persona', { //define un nuevo modelo, representando una tabla en la DB
     name: {
         type: DataTypes.STRING
     },
